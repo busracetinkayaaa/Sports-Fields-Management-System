abd9f0c0dc6da86c1ad27fcb81ce71e6a61aa13d
busracetinkayaaa <ceng.busracetinkaya@gmail.com>
2026-01-19 17:08:23 +0300

    chore: boilerplate setup, install dependencies and verify build

---
+++ src/main/java/com/app/SportsFieldManagement/SportsFieldManagementApplication.java
@@ -0,0 +1,13 @@
+package com.app.SportsFieldManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsFieldManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsFieldManagementApplication.class, args);
+	}
+
+}
