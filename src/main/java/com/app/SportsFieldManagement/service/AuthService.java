dd4ca9bc13bf1b182337d739ada2701c9a300d45
busracetinkayaaa <ceng.busracetinkaya@gmail.com>
2026-01-22 22:27:02 +0300

    feat: add business logic for authentication, including password encoding and token generation

---
+++ src/main/java/com/app/SportsFieldManagement/service/AuthService.java
@@ -0,0 +1,50 @@
+package com.app.SportsFieldManagement.service;
+
+import com.app.SportsFieldManagement.dto.LoginRequest;
+import com.app.SportsFieldManagement.dto.LoginResponse;
+import com.app.SportsFieldManagement.dto.RegisterRequest;
+import com.app.SportsFieldManagement.dto.UserResponse;
+import com.app.SportsFieldManagement.mapper.UserMapper;
+import com.app.SportsFieldManagement.model.User;
+import com.app.SportsFieldManagement.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class AuthService {
+    private final AuthenticationManager authenticationManager;
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+    private final TokenService tokenService;
+    private final PasswordEncoder passwordEncoder;
+    public UserResponse register(RegisterRequest request){
+        log.info("Register attempt for username: {}", request.username());
+        if(userRepository.existsByUsername(request.username())){
+            log.warn("Register failed - username already exists: {}", request.username());
+            throw new RuntimeException("Username exists");
+        }
+        String encoded = passwordEncoder.encode(request.password());
+        User user =userMapper.toEntity(request,encoded);
+        userRepository.save(user);
+        log.info("User registered successfully. username={}, role={}", user.getUsername(), user.getRole());
+        return userMapper.toResponse(user);
+    }
+
+    public LoginResponse login(LoginRequest request) {
+        log.info("Login attempt for username: {}", request.username());
+        Authentication authentication=authenticationManager.authenticate( new UsernamePasswordAuthenticationToken(
+                request.username(), request.password()
+        ));
+        log.info("Login successful for username: {}", authentication.getName());
+        String token = tokenService.generateToken(authentication);
+        log.debug("JWT token generated for username: {}", authentication.getName());
+        return new LoginResponse(token);
+    }
+}
