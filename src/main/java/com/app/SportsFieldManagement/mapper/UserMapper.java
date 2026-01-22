2b88d8bb5a75c5840ac0218dc087d23763a0ff57
busracetinkayaaa <ceng.busracetinkaya@gmail.com>
2026-01-22 22:19:15 +0300

    feat: add UserMapper for Entity and DTO conversions

---
+++ src/main/java/com/app/SportsFieldManagement/mapper/UserMapper.java
@@ -0,0 +1,20 @@
+package com.app.SportsFieldManagement.mapper;
+
+import com.app.SportsFieldManagement.dto.RegisterRequest;
+import com.app.SportsFieldManagement.dto.UserResponse;
+import com.app.SportsFieldManagement.model.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper {
+    public UserResponse toResponse(User user) {
+        return new UserResponse(user.getId(),user.getUsername(),user.getRole());
+    }
+    public User toEntity(RegisterRequest request,String encodedPassword){
+        return User.builder().username(request.username())
+                .password(encodedPassword)
+                .role("USER").build();
+    }
+
+}
