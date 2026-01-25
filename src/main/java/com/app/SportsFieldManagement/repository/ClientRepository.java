5dfcad7aa45cdee0a5fa512d2f10fb0cea0cc668
busracetinkayaaa <ceng.busracetinkaya@gmail.com>
2026-01-26 02:56:03 +0300

    feat: implement specific database queries for reservation conflict checks and client lookups

--- src/main/java/com/app/SportsFieldManagement/repository/ClientRepository.java
+++ src/main/java/com/app/SportsFieldManagement/repository/ClientRepository.java
@@ -1,13 +1,17 @@
 package com.app.SportsFieldManagement.repository;
 
 import com.app.SportsFieldManagement.model.Client;
+import com.app.SportsFieldManagement.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface ClientRepository extends JpaRepository<Client,Long> {
+    boolean existsByUser(User user);
     Optional<Client> findById(Long id);
     Optional<Client> findByPhone(String phone);
     List<Client> findAll();
+    Optional<Client> findByUserId(Long userId);
+    Optional<Client> findByUserUsername(String username);
 }
--- src/main/java/com/app/SportsFieldManagement/repository/ReservationRepository.java
+++ src/main/java/com/app/SportsFieldManagement/repository/ReservationRepository.java
@@ -1,19 +1,24 @@
 package com.app.SportsFieldManagement.repository;
 
+import com.app.SportsFieldManagement.model.Client;
 import com.app.SportsFieldManagement.model.Reservation;
 import com.app.SportsFieldManagement.model.SportField;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 public interface ReservationRepository extends JpaRepository<Reservation,Long> {
-    Optional<Reservation> getReservationsByDate(LocalDateTime startHour);
     List<Reservation> findAll();
+    @Query("SELECT r FROM Reservation r WHERE r.field=:field AND DATE(r.startHour)=:date")
+    List<Reservation> findByFieldAndDate(SportField field, LocalDate date);
     @Query("SELECT r FROM Reservation r WHERE r.field= :field AND r.endHour>:start AND r.startHour<:end")
-    List<Reservation> findConflictingReservations(@Param("field")SportField field,
-                                                  @Param("start") LocalDateTime start, @Param("end") LocalDateTime end);
+    boolean findConflictingReservations(@Param("field")SportField field,
+                                                  @Param("startHour") LocalDateTime startHour, @Param("endHour") LocalDateTime endHour);
+   List<Reservation> findByField(SportField field);
+   List<Reservation>findByClient(Client client);
+
 }
